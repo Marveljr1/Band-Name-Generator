8ae6bce2e7f280b68f5b9a2e390e361d0a2a95fa
Keith Williams <kwilliams623@hotmail.com>
2023-11-30 17:43:03 -0500

    first commit

---
+++ Band Name Generator/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!!")
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your pet growing up?\n")
+print("Your Band Name Should Be " + city + " " + pet)            
